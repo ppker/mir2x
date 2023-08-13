56098f77e4f81d3ce8f46efa6c7a77d6f740f5df
etorth <anhonghe@gmail.com>
2023-08-13 12:57:35 -0700

    updated all quest scripts

--- server/script/quest_backup/升级奖励.lua
+++
@@ -1,8 +0,0 @@
-function main()
-    addQuestTrigger(SYS_ON_LEVELUP, function(playerUID, oldLevel, newLevel)
-        uidExecute(playerUID,
-        [[
-            postString([=[恭喜你从%d升到了%d级！]=])
-        ]], oldLevel, newLevel)
-    end)
-end
