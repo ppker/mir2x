04e44008f8fa5e24f0fe204b5e24803e0b2c469a
etorth <anhonghe@gmail.com>
2023-08-22 23:11:39 -0700

    f

--- server/script/api/qst.lua
+++ server/script/api/qst.lua
@@ -1,5 +1,15 @@
 local qst = {}
 
-function qst.getQuestName(uid) return uidRemoteCall(uid, [[ return getQuestName() ]]) end
+function qst.getQuestName(questUID)
+    return uidRemoteCall(questUID, [[ return getQuestName() ]])
+end
+
+function qst.getQuestState(questUID, playerUID, fsmName)
+    return uidRemoteCall(questUID, playerUID, fsmName,
+    [[
+        local playerUID, fsmName = ...
+        return dbGetUIDQuestState(playerUID, fsmName)
+    ]])
+end
 
 return qst
