b7a8530413a2e9e3ef44311d47269782e64ade2e
etorth <anhonghe@gmail.com>
2023-09-04 17:42:43 -0700

    f

--- server/script/api/npc.lua
+++ server/script/api/npc.lua
@@ -3,4 +3,24 @@ function npc.getUID(mapName, npcName)
     assertType(mapName, 'string')
     assertType(npcName, 'string')
 end
+
+function npc.runHandler(npcUID, playerUID, eventPath, event, value)
+    assertType(npcUID, 'integer')
+    assert(isNPChar(npcUID))
+
+    assertType(playerUID, 'integer')
+    assert(isPlayer(playerUID))
+
+    assertType(eventPath, 'array')
+    assertType(eventPath[1], 'string')
+
+    assertType(event, 'string')
+    assertType(value, 'string', 'nil')
+
+    uidRemoteCall(npcUID, playerUID, eventPath, event, value,
+    [[
+        local playerUID, eventPath, event, value = ...
+        runEventHandler(playerUID, eventPath, event, value)
+    ]])
+end
 return npc
--- server/script/quest/石母任务.lua
+++ server/script/quest/石母任务.lua
@@ -1,3 +1,22 @@
+uidRemoteCall(getNPCharUID('比奇县_0_003', '石母_1'), getUID(), getQuestName(),
+[[
+    local questUID, questName = ...
+    local questPath = {SYS_EPQST, questName}
+
+    setQuestHandler(questName,
+    {
+        [SYS_ENTER] = function(uid, args)
+            uidPostXML(playerUID, questPath,
+            [=[
+                <layout>
+                    <par>这到底是怎么回事？那就是说女人回来了？</par>
+                    <par><event id="%s">退出</event></par>
+                </layout>
+            ]=], SYS_EXIT)
+        end,
+    })
+]])
+
 uidRemoteCall(getNPCharUID('比奇县_0', '母子石像_1'), getUID(), getQuestName(),
 [[
     local questUID, questName = ...
@@ -8,19 +27,7 @@ uidRemoteCall(getNPCharUID('比奇县_0', '母子石像_1'), getUID(), getQuestN
         [SYS_CHECKACTIVE] = false,
         [SYS_ENTER] = function(uid, args)
             plyapi.spaceMove(uid, '比奇县_0_003', 28, 35)
-            uidRemoteCall(getNPCharUID('比奇县_0_003', '石母_1'), uid, questUID, questName,
-            [=[
-                local playerUID, questUID, questName = ...
-                local questPath = {SYS_EPUID, questName}
-
-                uidPostXML(playerUID, questPath,
-                [==[
-                    <layout>
-                        <par>这到底是怎么回事？那就是说女人回来了？</par>
-                        <par><event id="%s">退出</event></par>
-                    </layout>
-                ]==], SYS_EXIT)
-            ]=])
+            npcapi.runHandler(getNPCharUID('比奇县_0_003', '石母_1'), uid, {SYS_EPQST, questName}, SYS_ENTER)
         end,
     })
 
@@ -39,3 +46,4 @@ uidRemoteCall(getNPCharUID('比奇县_0', '母子石像_1'), getUID(), getQuestN
         ]=], SYS_ENTER, SYS_EXIT)
     end)
 ]])
+
