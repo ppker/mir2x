144445938a784f78d83591de7452358fa2ac6ac4
etorth <anhonghe@gmail.com>
2023-09-04 18:32:34 -0700

    f

--- server/script/quest/石母任务.lua
+++ server/script/quest/石母任务.lua
@@ -1,7 +1,39 @@
+setQuestFSMTable(
+{
+    [SYS_ENTER] = function(uid, args)
+        setupNPCQuestBehavior('比奇县_0_003', '石母_1', uid,
+        [[
+            return getQuestName()
+        ]],
+        [[
+            local questName = ...
+            local questPath = {SYS_EPUID, questName}
+
+            return
+            {
+                [SYS_ENTER] = function(uid, args)
+                    uidPostXML(uid, questPath,
+                    [=[
+                        <layout>
+                            <par>还没有找到我孩子吗？</par>
+                            <par><event id="%s">结束</event></par>
+                        </layout>
+                    ]=], SYS_EXIT)
+                end,
+            }
+        ]])
+    end,
+})
+
 uidRemoteCall(getNPCharUID('比奇县_0_003', '石母_1'), getUID(), getQuestName(),
 [[
     local questUID, questName = ...
     local questPath = {SYS_EPQST, questName}
+
+    local fnLeaveMap = function(uid)
+        local x, y = uidRemoteCall(uid, [=[ return getMapLoc() ]=])
+        plyapi.spaceMove(uid, '比奇县_0', x + 490, y + 360)
+    end
 
     setQuestHandler(questName,
     {
@@ -10,15 +42,59 @@ uidRemoteCall(getNPCharUID('比奇县_0_003', '石母_1'), getUID(), getQuestNam
             [=[
                 <layout>
                     <par>这位侠客，请一定要帮帮我啊！</par>
-                    <par><event id="npc_exit">没听见没听见，我走了！</event></par>
+                    <par><event id="npc_leave">假装没听见，我走了！</event></par>
                     <par><event id="npc_ask">什么事啊？</event></par>
                 </layout>
             ]=])
         end,
 
-        npc_exit = function(uid, args)
-            local x, y = uidRemoteCall(uid, [=[ return getMapLoc() ]=])
-            plyapi.spaceMove(uid, '比奇县_0', x + 490, y + 360)
+        npc_leave = function(uid, args)
+            uidPostXML(uid, questPath,
+            [=[
+                <layout>
+                    <par>请一定要帮帮我...啊？太无情了！</par>
+                    <par><event id="%s">结束</event></par>
+                </layout>
+            ]=], SYS_EXIT)
+
+            fnLeaveMap(uid)
+        end,
+
+        npc_ask = function(uid, args)
+            uidPostXML(uid, questPath,
+            [=[
+                <layout>
+                    <par>呜呜...不久前我的孩子在夜市被商人给抢走了。一定要帮我找回孩子啊！</par>
+                    <par>不知道他把孩子带走到底想干什么...一定要...拜托您了！</par>
+                    <par><event id="npc_accept">知道了，我会去帮你找回孩子的！</event></par>
+                    <par><event id="npc_refuse">我没有这闲工夫。</event></par>
+                </layout>
+            ]=], SYS_EXIT)
+        end,
+
+        npc_accept = function(uid, args)
+            uidPostXML(uid, questPath,
+            [=[
+                <layout>
+                    <par>多谢了！听说夜市的商人在(452,297)，请一定要帮我找回孩子啊！</par>
+                    <par><event id="%s">结束</event></par>
+                </layout>
+            ]=], SYS_EXIT)
+
+            fnLeaveMap(uid)
+            qstapi.setState(questUID, {uid=uid, state=SYS_ENTER})
+        end,
+
+        npc_refuse = function(uid, args)
+            uidPostXML(uid, questPath,
+            [=[
+                <layout>
+                    <par>这样啊...呜呜...天下之大，竟然没有同情失去孩儿母亲心的侠客吗？...</par>
+                    <par><event id="%s">结束</event></par>
+                </layout>
+            ]=], SYS_EXIT)
+
+            fnLeaveMap(uid)
         end,
 
         npc_extra = function(uid, args)
@@ -63,4 +139,3 @@ uidRemoteCall(getNPCharUID('比奇县_0', '母子石像_1'), getUID(), getQuestN
         ]=], SYS_ENTER, SYS_EXIT)
     end)
 ]])
-
