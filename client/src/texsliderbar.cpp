59a7f82c8da0d4a891c87a428a9c7100fe2f02b6
etorth <anhonghe@gmail.com>
2023-11-27 18:50:52 -0800

    f

--- client/src/texsliderbar.cpp
+++ client/src/texsliderbar.cpp
@@ -36,8 +36,8 @@ TexSliderBar::TexSliderBar(
           w() / 2,
           h() / 2,
 
-          w(),
-          h(),
+          w() - 6,
+          h() - 6,
 
           argHSlider,
           argSliderIndex,
@@ -169,8 +169,8 @@ TexSliderBar::TexSliderBar(
           /* x */  argHSlider ? 3 : 2,
           /* y */ !argHSlider ? 3 : 2,
 
-          /* w */  argHSlider ? w() - 6 : m_barImage.w(),
-          /* h */ !argHSlider ? h() - 6 : m_barImage.h(),
+          /* w */  argHSlider ? to_d(std::lround((w() - 6) * m_slider.getValue())) : m_barImage.w(),
+          /* h */ !argHSlider ? to_d(std::lround((h() - 6) * m_slider.getValue())) : m_barImage.h(),
 
           &m_barCrop,
 
