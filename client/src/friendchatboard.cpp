bcf724fca85d856dd5c3dd266e21aa5414f521c3
etorth <anhonghe@gmail.com>
2024-03-27 00:19:22 -0700

    f

--- client/src/friendchatboard.cpp
+++ client/src/friendchatboard.cpp
@@ -615,7 +615,7 @@ struct FriendChatPage: public Widget
             //
             // layout always attach to buttom of input container, so argX needs container height
             // in initialization list we can not call this->h() since initialization of layout is not done yet
-            layout.moveAt(DIR_DOWNLEFT, 0, this->h() - 1);
+            layout.moveAt(DIR_DOWNLEFT, 0, [this](const Widget *){ return this->h() - 1; });
         }
     };
 
